3e3de0b6bb28694fe50b68fbd96d396b588639e4
n4-t3 <nathanmekuria@gmail.com>
2022-10-10 00:24:54 +0900

    created navbar and menu card

---
+++ src/pages/menu/menu.jsx
@@ -0,0 +1,14 @@
+import menuCSS from './menu.module.scss'
+import Card from '../../components/card/card.component'
+const Menu = (props) =>{
+    return(
+    <div className={menuCSS.wrapper}>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+
+    </div>
+    )
+}
+
+export default Menu
