380a4d98733f30a00ac1f55488ab3776e71197df
n4-t3 <nathanmekuria@gmail.com>
2022-10-20 16:02:50 +0900

    added DRF API URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://127.0.0.1:8000/"
+export const baseURL = "http://nathan34.pythonanywhere.com/"
 
 let authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
 
